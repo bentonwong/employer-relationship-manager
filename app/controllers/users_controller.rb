86df383281577b0ecd8634880c3f4a50f2357236
bentonwong <bentonwong@gmail.com>
2017-08-21 14:26:49 -0700

    code user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,48 @@
 class UsersController < ApplicationController
+  def index
+    @users = Users.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+  end
+
+  def edit
+    redirect_to user_path(current_user) if session[:user_id] != params[:id].to_i
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to @user
+  end
+
+  def show
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def set_user
+      @user = User.find_by(id: params[:id])
+    end
+
+    def save_user
+      if @user.save
+        if !sign_in?
+          session.clear
+          session[:user_id] = @user.id
+        end
+        redirect_to @lawyer
+      else
+        render :new
+      end
+    end
+    
 end
