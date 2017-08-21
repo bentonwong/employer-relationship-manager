6f7c5308f479f2a95bc91b882bc8e86d8b70e89b
bentonwong <bentonwong@gmail.com>
2017-08-21 14:54:17 -0700

    add applications controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,2 +1,39 @@
 class ApplicationsController < ApplicationController
+
+  def index
+    @applications = Application.all
+  end
+
+  def new
+    @application = Application.new
+  end
+
+  def create
+    @application = Application.create(application_params)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+    @application.destroy
+    redirect_to applications_path
+  end
+
+  private
+
+    def application_params
+      params.require(:applications).permit(:date, :status, :job_id, :notes)
+    end
+
+    def set_application
+      @application = Application.find_by(id: params[:id])
+    end
+
 end
