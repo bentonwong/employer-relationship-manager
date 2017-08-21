dad64cd92d835283b95f24180d514458c7ca73b7
bentonwong <bentonwong@gmail.com>
2017-08-21 15:08:15 -0700

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_many :applications
+  has_many :jobs, through: :applications
+  validates :name, :email, presence: :true
+  validates :email, uniqueness: :true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  validates :password, length: { minimum: 8 }, allow_nil: true
+  has_secure_password
+  
 end
