aa8bdd5bcb47d04f6a1858b471fe5b5cf367e6e2
bentonwong <bentonwong@gmail.com>
2017-08-21 14:46:23 -0700

    add current_user and signed_in to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,17 @@
 module ApplicationHelper
 
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
   private
 
     def authorized?
       redirect_to root_path if !signed_in?
     end
+
 end
